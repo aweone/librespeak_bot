f0418bbe02a7f35b91229506a994bf44341044e0
Paul <pavlhusnullin765@gmail.com>
2021-03-21 13:38:10 +0500

    вынес в отдельный файл

---
+++ chatAdmin.py
@@ -0,0 +1,20 @@
+from auth import vk
+import random
+
+def get_admin(peerid, groupid, event):
+    try:
+        admins = []
+        admins_str = ""
+        members = vk.messages.getConversationMembers(peer_id=peerid, group_id=groupid)["items"]
+        for member in members:
+            if "is_admin" in member:
+                admins.append(member["member_id"])
+                if member["member_id"] > 0:
+                    admins_str+=("\n @id"+str(member["member_id"]))
+                #print(member["member_id"])
+        #print(members)
+        return admins_str, admins
+    except:
+        admins = []
+        admins_str = ""
+        return admins_str, admins
