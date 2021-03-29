e8befe1cb981a3e992510e2a41437a9770446a3c
Paul <pavlhusnullin765@gmail.com>
2021-03-29 23:49:53 +0500

    Initial commit

---
+++ qrDecode.py
@@ -0,0 +1,14 @@
+from PIL import Image
+from pyzbar.pyzbar import decode
+import requests
+
+def qrdecode(link):
+    r = requests.get(link)
+    with open('decodeqr.png', 'wb') as f:
+        f.write(r.content) 
+    data = decode(Image.open('decodeqr.png'))
+    if data:
+        return data[0].data.decode("utf-8")
+    elif data == []:
+        return ""
+
