ffc8bdaf13f48783fd1e660a3e53521d3f5f3fc8
Paul <pavlhusnullin765@gmail.com>
2021-03-31 21:17:50 +0500

    Upload wiki.py

---
+++ wiki.py
@@ -0,0 +1,15 @@
+import wikipedia
+from requests import get
+def Wiki(requestName, sentences=4):
+    try:
+        wikipedia.set_lang("ru")
+        page = wikipedia.page(requestName)
+        text = wikipedia.summary(requestName, sentences)
+        title = page.title
+        similar = "\n".join(wikipedia.search(requestName))
+        url = get("https://clck.ru/--?url=" + page.url).text
+    except:
+        return "По вашему запросу ничего не найдено!"
+    
+    return f"{title}\n{text}\nПодробнее: {url}\n\nПохожие запросы:\n{similar}"
+
