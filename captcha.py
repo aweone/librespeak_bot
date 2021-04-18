f345453a18d98aab8c55ff094926dfc2abdfbe72
Evgeniy Rybin <z.ribin20@gmail.com>
2021-04-18 14:52:10 +0500

    Рефакторит captcha.py

--- captcha.py
+++ captcha.py
@@ -9,77 +9,13 @@ from color_list import colors
 from warnings import warn
 
 
-username = str(Path.home())
-
-char_list = []
-
-for i in range(65, 91):
-    char_list.append(chr(i))
-for i in range(97, 123):
-    char_list.append(chr(i))
-for i in range(48, 58):
-    char_list.append(chr(i))
-
-font = ImageFont.truetype('font/Cantarell-Bold.otf', 110)
-
-
-def __get_captcha():
-    warn("Используется устаревший модуль")
-    warn
-    captcha = ""
-    for _ in range(6):
-        captcha += random.choice(char_list)
-
-    print(captcha)
-
-    im = Image.open("images/captcha.jpg")
-    im1 = Image.open("images/captcha4.jpg")
-    im2 = Image.open("images/captcha5.jpg")
-    im3 = Image.open("images/captcha6.jpg")
-    im4 = Image.open("images/captcha2.jpg")
-    im5 = Image.open("images/captcha7.jpg")
-    draw_text = ImageDraw.Draw(im)
-    wText, _hText = draw_text.textsize(captcha, font)
-    wIm, _hIm = im.size
-    # print(wIm, hIm, _wText, _hText)
-    draw_text.text(
-        ((wIm-wText)/2, 160),
-        str(captcha),
-        font=font,
-        fill=(0, 0, 0, 128)
-    )
-    Image.blend(
-        Image.blend(
-            Image.blend(
-                Image.blend(
-                    Image.blend(
-                        im, im1, 50),
-                    im2, 50),
-                im3, 50),
-            im4, 50),
-        im5, 50).save('images/captcha3.jpg')
-
-    upload = vk_api.VkUpload(vk)
-    photo = upload.photo_messages("images/captcha3.jpg")
-    owner_id = photo[0]['owner_id']
-    photo_id = photo[0]['id']
-    access_key = photo[0]['access_key']
-    attachment = f'photo{owner_id}_{photo_id}_{access_key}'
-    print(attachment)
-    return attachment, captcha
-
-
 chars = string.ascii_lowercase + string.digits
 
 
 def get_captcha():
     n = 0
     font = ImageFont.truetype('font/Cantarell-Bold.otf', 70)
-    captcha = ""
-    for i in range(6):
-        captcha += random.choice(chars)
-
-    print(captcha)
+    captcha = "".join([random.choice(chars) for _ in range(6)])
 
     im = Image.new('RGBA', size=(400, 200), color=(255, 255, 255, 255))
     draw = ImageDraw.Draw(im)
@@ -106,7 +42,6 @@ def get_captcha():
             random.randint(1, 600), random.randint(1, 400),
             random.randint(1, 600), random.randint(1, 400)),
             fill=random.choice(colors)
-
         )
 
     for i in range(0, random.randint(700, 1500)):
@@ -119,10 +54,9 @@ def get_captcha():
 
     im.save("captcha.png", "PNG")
     upload = vk_api.VkUpload(vk)
-    photo = upload.photo_messages("captcha.png")
-    owner_id = photo[0]['owner_id']
-    photo_id = photo[0]['id']
-    access_key = photo[0]['access_key']
+    photo = upload.photo_messages("captcha.png")[0]
+    owner_id = photo['owner_id']
+    photo_id = photo['id']
+    access_key = photo['access_key']
     attachment = f'photo{owner_id}_{photo_id}_{access_key}'
-    print(attachment)
     return attachment, captcha
